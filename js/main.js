a93651c0f8fa5370a82e7354da3bdfd11447ee44
Rpg87 <sra.picadillo@gmail.com>
2022-10-21 11:30:05 +0200

    trying if/else

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
 'use strict';
 
 const jsNewForm = document.querySelector('.js-new-form');
-jsNewForm.classList.remove("collapsed");
-
-
 
 
 const newKitten = document.querySelector('.js-list');
 
 
+// const de los gatos con sus características 
 const imageCat1 =  'https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg';
 const nameCat1 = 'anastacio'.toUpperCase();
 const race1 = 'British Shorthair' ;
@@ -27,7 +25,6 @@ const description3 = 'Ruiseñor, juguetón, le guta estar tranquilo y que nadie 
 
 /*jsList.innerHTML = `<li> <img src=" ${imageCat1}"> <h3>${nameCat1}</h3> <h4>${race1}</h4> <p>${description1}</p>  </li>`;*/
 
-/* <li class="card"> <article> <img class="card_img" src="https://ychef.files.bbci.co.uk/976x549/p07ryyyj.jpg" alt="gatito" /> <h3 class="card_title">Anastacio</h3> <h4 class="card_race">British Shorthair</h4> <p class="card_description">    Ruiseño, juguetón, le guta estar tranquilo y que nadie lemoleste. Es una maravilla acariciarle! </p> </article> </li> */
 
 const kitten1 = 
 `<li class="card"><article><img class="card_img" src="${imageCat1}" alt="gatito" /><h3 class="card_title">${nameCat1} </3><h4class="card_race">${race1} </h4><p class="card_description">${description1} </p></article></li>`;
@@ -38,9 +35,36 @@ const kitten2 =
 const kitten3 = 
 `<li class="card"><article><img class="card_img" src="${imageCat3}" alt="gatito" /><h3 class="card_title">${nameCat3} </3><h4class="card_race">${race3} </h4><p class="card_description">${description3} </p></article></li>`;
 
+//calls 
+const input_search_desc = document.querySelector('.js_in_search_desc'); //línea 57 html
+
+// Actions 
+jsNewForm.classList.remove('collapsed');
+input_search_desc.value = 'cariñoso';
+const descrSearchText = input_search_desc.value;
+
+// HOLA VICKY!!! punto nº5 del ejercicio. Entendí q debe verse el kitten1/kitten2... según si sale la palabra cariñoso y estaba justo revisando la lecci´n pq me cuesta. Lo revisamos juntas el lunes con calma si se puede. Presión 0!!
+/*if( kitten1.includes(descrSearchText) ) {
+    else if 
+
+
+ { 
+    
+    else { kitten1.classList.add('collapsed'); 
+    }    } */
+    
+    if( kittenDesc2.includes(descrSearchText) ) {
+    //Completa el código
+    }
+    
+    if( kittenDesc3.includes(descrSearchText) ) {
+    //Completa el código
+    }
+        
+
 //const catRace = document.querySelector('.card_race'); <-- revisar este punto 
 // hemos añadido la clase después de card_race y no funciona por qué
-//catRace.classList.add('desaparecer');
+
 newKitten.innerHTML = kitten1 + kitten2 + kitten3;
 
 
